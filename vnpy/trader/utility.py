3bd4c229bf93a8a0d37a55b2ce837cfaee1cfa31
siaosing <jingsiaosing@gmail.com>
2022-03-22 14:16:03 +0800

    add NewBarGenerator

--- vnpy/trader/utility.py
+++ vnpy/trader/utility.py
@@ -18,7 +18,15 @@ from .constant import Exchange, Interval
 
 
 log_formatter = logging.Formatter('[%(asctime)s] %(message)s')
-
+# add instrument_night_xxxx
+instrument_night_2300 = [
+    'A', 'B', 'M', 'Y', 'I', 'JM', 'J', 'P', 'C', 'CS', 'RR', 'EG', 'EB', 'L',
+    'V', 'PP', 'PG', 'RM', 'OI', 'SA', 'TA', 'CF', 'CY', 'SR', 'FG', 'ZC',
+    'MA', 'RB', 'HC', 'BU', 'RU', 'SP', 'FU', 'LU', 'NR'
+]
+instrument_night_0100 = ['CU', 'AL', 'ZN', 'PB', 'NI', 'SN', 'SS']
+instrument_night_0230 = ['AU', 'AG', 'SC']
+instrument_cffex = ['IF', 'IC', 'IH', 'T', 'TF']
 
 def extract_vt_symbol(vt_symbol: str) -> Tuple[str, Exchange]:
     """
@@ -430,6 +438,325 @@ class BarGenerator:
 
         self.bar = None
         return bar
+
+class NewBarGenerator:   # add NewBarGenerator
+    """
+    For:
+    1. generating 1 minute bar data from tick data
+    2. generating x minute bar/x hour bar data from 1 minute data
+
+    Notice:
+    1. for x minute bar, x must be able to divide 60: 2, 3, 5, 6, 10, 15, 20, 30
+    2. for x hour bar, x can be any number
+    """
+
+    def __init__(
+        self,
+        on_bar: Callable,
+        window: int = 0,
+        on_window_bar: Callable = None,
+        interval: Interval = Interval.MINUTE
+    ):
+        """Constructor"""
+        self.bar: BarData = None
+        self.on_bar: Callable = on_bar
+
+        self.interval: Interval = interval
+        self.interval_count: int = 0
+
+        self.hour_bar: BarData = None
+
+        self.window: int = window
+        self.window_bar: BarData = None
+        self.on_window_bar: Callable = on_window_bar
+
+        self.last_tick: TickData = None
+
+    def update_tick(self, tick: TickData) -> None:
+        """
+        Update new tick data into generator.
+        """
+        new_minute = False
+
+        # Filter tick data with 0 last price
+        if not tick.last_price:
+            return
+
+        # Filter tick data with older timestamp
+        if self.last_tick and tick.datetime < self.last_tick.datetime:
+            return
+
+        if not self.bar:
+            new_minute = True
+        elif (
+            (self.bar.datetime.minute != tick.datetime.minute)
+            or (self.bar.datetime.hour != tick.datetime.hour)
+        ):
+            session_ending = False
+            if tick.datetime.strftime('%H:%M:%S') in ['23:00:00', '01:00:00', '02:30:00', '10:15:00', '11:30:00']:
+                session_ending = self.check_session_ending(tick)
+            if not session_ending:
+                self.bar.datetime = self.bar.datetime.replace(
+                    second=0, microsecond=0
+                ) + timedelta(minutes=1)  # 向后归结 和rq一致
+                self.on_bar(self.bar)
+
+                new_minute = True
+
+        if new_minute and not self.last_tick:  # first tick defer to the next minute bar
+            self.bar = BarData(
+                symbol=tick.symbol,
+                exchange=tick.exchange,
+                interval=Interval.MINUTE,
+                datetime=tick.datetime + timedelta(minutes=1),
+                gateway_name=tick.gateway_name,
+                open_price=tick.last_price,
+                high_price=tick.last_price,
+                low_price=tick.last_price,
+                close_price=tick.last_price,
+                twap=tick.last_price,
+                ticks=[tick.last_price],
+                open_interest=tick.open_interest
+            )
+        elif new_minute and self.last_tick:
+            self.bar = BarData(
+                symbol=tick.symbol,
+                exchange=tick.exchange,
+                interval=Interval.MINUTE,
+                datetime=tick.datetime,
+                gateway_name=tick.gateway_name,
+                open_price=tick.last_price,
+                high_price=tick.last_price,
+                low_price=tick.last_price,
+                close_price=tick.last_price,
+                twap=tick.last_price,
+                ticks=[tick.last_price],
+                open_interest=tick.open_interest
+            )
+        else:
+            self.bar.high_price = max(self.bar.high_price, tick.last_price)
+            if tick.high_price > self.last_tick.high_price:
+                self.bar.high_price = max(self.bar.high_price, tick.high_price)
+
+            self.bar.low_price = min(self.bar.low_price, tick.last_price)
+            if tick.low_price < self.last_tick.low_price:
+                self.bar.low_price = min(self.bar.low_price, tick.low_price)
+
+            self.bar.close_price = tick.last_price
+            self.bar.open_interest = tick.open_interest
+            self.bar.ticks.append(tick.last_price)
+            self.bar.twap = sum(self.bar.ticks) / len(self.bar.ticks)
+            if not self.check_session_ending:
+                self.bar.datetime = tick.datetime
+
+        if self.last_tick:
+            volume_change = tick.volume - self.last_tick.volume
+            self.bar.volume += max(volume_change, 0)
+
+            turnover_change = tick.turnover - self.last_tick.turnover
+            self.bar.turnover += max(turnover_change, 0)
+        else:
+            self.bar.volume = tick.volume
+            self.bar.turnover = tick.turnover
+        if self.bar.volume:
+            self.bar.vwap = self.bar.turnover / self.bar.volume
+
+        self.last_tick = tick
+
+    def check_session_ending(self, tick: TickData) -> None:
+        if tick.datetime.strftime('%H:%M') == '11:30':
+            return True
+        if tick.datetime.strftime('%H:%M') == '02:30':
+            return True
+        if re.sub('\d+', '', tick.symbol.upper()) in instrument_night_2300 and tick.datetime.strftime('%H:%M') == '23:00':
+            return True
+        if re.sub('\d+', '', tick.symbol.upper()) in instrument_night_0100 and tick.datetime.strftime('%H:%M') == '01:00':
+            return True
+        if re.sub('\d+', '', tick.symbol.upper()) not in instrument_cffex and tick.datetime.strftime('%H:%M') == '10:15':
+            return True
+
+        else:
+            return False
+
+    def update_bar(self, bar: BarData) -> None:
+        """
+        Update 1 minute bar into generator
+        """
+        if self.interval == Interval.MINUTE:
+            self.update_bar_minute_window(bar)
+        else:
+            self.update_bar_hour_window(bar)
+
+    def update_bar_minute_window(self, bar: BarData) -> None:
+        """"""
+        dt = bar.datetime.replace(second=0, microsecond=0)
+        # If not inited, create window bar object
+        if not self.window_bar:
+            dt = bar.datetime.replace(second=0, microsecond=0)
+            self.window_bar = BarData(
+                symbol=bar.symbol,
+                exchange=bar.exchange,
+                datetime=dt,
+                gateway_name=bar.gateway_name,
+                open_price=bar.open_price,
+                high_price=bar.high_price,
+                low_price=bar.low_price
+            )
+        # Otherwise, update high/low price into window bar
+        else:
+            self.window_bar.high_price = max(
+                self.window_bar.high_price,
+                bar.high_price
+            )
+            self.window_bar.low_price = min(
+                self.window_bar.low_price,
+                bar.low_price
+            )
+
+        # Update close price/volume/turnover into window bar
+        self.window_bar.ticks.extend(bar.ticks)
+        self.window_bar.close_price = bar.close_price
+        self.window_bar.twap = sum(self.window_bar.ticks) / len(self.window_bar.ticks)
+        self.window_bar.volume += bar.volume
+        self.window_bar.turnover += bar.turnover
+        self.window_bar.open_interest = bar.open_interest
+        self.window_bar.datetime = dt
+        if self.window_bar.volume:
+            self.window_bar.vwap = self.window_bar.turnover / self.window_bar.volume
+
+        # Check if window bar completed
+        if not (bar.datetime.minute) % self.window:
+            self.on_window_bar(self.window_bar)
+            self.window_bar = None
+
+    def update_bar_hour_window(self, bar: BarData) -> None:
+        """"""
+        # If not inited, create window bar object
+        if not self.hour_bar:
+            dt = bar.datetime.replace(minute=0, second=0, microsecond=0)
+            self.hour_bar = BarData(
+                symbol=bar.symbol,
+                exchange=bar.exchange,
+                datetime=dt,
+                gateway_name=bar.gateway_name,
+                open_price=bar.open_price,
+                high_price=bar.high_price,
+                low_price=bar.low_price,
+                close_price=bar.close_price,
+                volume=bar.volume,
+                turnover=bar.turnover,
+                open_interest=bar.open_interest
+            )
+            return
+
+        finished_bar = None
+
+        # If minute is 59, update minute bar into window bar and push
+        if bar.datetime.minute == 59:
+            self.hour_bar.high_price = max(
+                self.hour_bar.high_price,
+                bar.high_price
+            )
+            self.hour_bar.low_price = min(
+                self.hour_bar.low_price,
+                bar.low_price
+            )
+
+            self.hour_bar.close_price = bar.close_price
+            self.hour_bar.volume += bar.volume
+            self.hour_bar.turnover += bar.turnover
+            self.hour_bar.open_interest = bar.open_interest
+
+            finished_bar = self.hour_bar
+            self.hour_bar = None
+
+        # If minute bar of new hour, then push existing window bar
+        elif bar.datetime.hour != self.hour_bar.datetime.hour:
+            finished_bar = self.hour_bar
+
+            dt = bar.datetime.replace(minute=0, second=0, microsecond=0)
+            self.hour_bar = BarData(
+                symbol=bar.symbol,
+                exchange=bar.exchange,
+                datetime=dt,
+                gateway_name=bar.gateway_name,
+                open_price=bar.open_price,
+                high_price=bar.high_price,
+                low_price=bar.low_price,
+                close_price=bar.close_price,
+                volume=bar.volume,
+                turnover=bar.turnover,
+                open_interest=bar.open_interest
+            )
+        # Otherwise only update minute bar
+        else:
+            self.hour_bar.high_price = max(
+                self.hour_bar.high_price,
+                bar.high_price
+            )
+            self.hour_bar.low_price = min(
+                self.hour_bar.low_price,
+                bar.low_price
+            )
+
+            self.hour_bar.close_price = bar.close_price
+            self.hour_bar.volume += bar.volume
+            self.hour_bar.turnover += bar.turnover
+            self.hour_bar.open_interest = bar.open_interest
+
+        # Push finished window bar
+        if finished_bar:
+            self.on_hour_bar(finished_bar)
+
+    def on_hour_bar(self, bar: BarData) -> None:
+        """"""
+        if self.window == 1:
+            self.on_window_bar(bar)
+        else:
+            if not self.window_bar:
+                self.window_bar = BarData(
+                    symbol=bar.symbol,
+                    exchange=bar.exchange,
+                    datetime=bar.datetime,
+                    gateway_name=bar.gateway_name,
+                    open_price=bar.open_price,
+                    high_price=bar.high_price,
+                    low_price=bar.low_price
+                )
+            else:
+                self.window_bar.high_price = max(
+                    self.window_bar.high_price,
+                    bar.high_price
+                )
+                self.window_bar.low_price = min(
+                    self.window_bar.low_price,
+                    bar.low_price
+                )
+
+            self.window_bar.close_price = bar.close_price
+            self.window_bar.volume += bar.volume
+            self.window_bar.turnover += bar.turnover
+            self.window_bar.open_interest = bar.open_interest
+
+            self.interval_count += 1
+            if not self.interval_count % self.window:
+                self.interval_count = 0
+                self.on_window_bar(self.window_bar)
+                self.window_bar = None
+
+    def generate(self) -> Optional[BarData]:
+        """
+        Generate the bar data and call callback immediately.
+        """
+        bar = self.bar
+
+        if self.bar:
+            bar.datetime = bar.datetime.replace(second=0, microsecond=0) + timedelta(minutes=1)  # 向后归结 和rq一致
+            self.on_bar(bar)
+
+        self.bar = None
+        return bar
+
 
 
 class ArrayManager(object):
